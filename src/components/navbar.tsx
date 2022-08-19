7f5c36e6d617759bb3a97add0d1d5a18f2916969
Alex <pauloalbuquerque@acm.org>
2022-08-19 17:38:05 -0300

    feat(navbar): adds `notImplemented` toast

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,16 @@
-import {Flex, Link, Spacer} from "@chakra-ui/react";
+import {Flex, Link, Spacer, useToast} from "@chakra-ui/react";
 import {NextComponentType} from "next";
 import {Search2Icon, SettingsIcon} from "@chakra-ui/icons";
 
 const Navbar: NextComponentType = () => {
+	const toast = useToast()
+	const notImplemented = () => toast({
+		title: 'Not Implemented',
+		description: 'This wasn\'t implemented yet.',
+		status: "error",
+		isClosable: true
+	})
+
 	return <Flex p={3} border='0px' borderBottom='2px' borderBottomColor='blackAlpha.100'>
 		<Link p={5} bg='red.100' fontSize={18}>
 			logo
@@ -12,8 +20,8 @@ const Navbar: NextComponentType = () => {
 
 		<Flex p={5} bg='green.100'>
 			<Link>
-				<Search2Icon w={5} h={5}/>
-				<SettingsIcon ml={4} w={5} h={5}/>
+				<Search2Icon onClick={notImplemented} w={5} h={5}/>
+				<SettingsIcon onClick={notImplemented} ml={4} w={5} h={5}/>
 			</Link>
 		</Flex>
 	</Flex>
